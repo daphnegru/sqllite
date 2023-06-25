e1d140017772f63de09b015f0e746b0fc3d2aaf4
dgruber <dgruber@outbrain.com>
2023-06-25 11:50:20 +0300

    Initial commit

---
+++ src/main/java/com/example/sqllite/SqlliteApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqllite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlliteApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SqlliteApplication.class, args);
+  }
+
+}
